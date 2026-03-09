45a3acf56c4f7a47dc3937f214a0eef109cb0259
sounicbehera <sounicbehera@gmail.com>
2026-03-09 23:58:49 +0530

    cors for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -52,7 +52,7 @@ const server = http.createServer(app);
 
 setupWebSocket(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
